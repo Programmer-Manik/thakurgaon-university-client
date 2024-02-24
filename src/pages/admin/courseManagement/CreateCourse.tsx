b20c11f7cf36d38c6b0a2129530778670a91ea13
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-24 14:18:30 +0600

    Handle Multiple Select

--- src/pages/admin/courseManagement/CreateCourse.tsx
+++ src/pages/admin/courseManagement/CreateCourse.tsx
@@ -1,9 +1,62 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
+import { toast } from "sonner";
+import PHInput from "../../../components/form/PHInput";
+import {  useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagement.api";
+
 const CreateCourse = () => {
+  // const [addSemester] = useAddRegisterSemesterMutation();
+
+  const { data: Courses } = useGetAllCoursesQuery(undefined)
+
+  const preRequisiteCoursesOption = Courses?.data?.map((item) => ({
+    value: item._id,
+    label: item.title,
+  }));
+  console.log(Courses);
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
+
+    const coursesData = {
+      ...data,
+      isDeleted:false,
+      preRequisiteCourses :data.preRequisiteCourses.map((item) => ({
+        Courses:item,
+        isDeleted:false,
+      }))
+    };
+
+    console.log(coursesData);
+
+  //   try {
+  //     const res = (await addSemester(semesterData)) as TResponse<any>;
+  //     console.log(res);
+  //     if (res.error) {
+  //       toast.error(res.error.data.message, { id: toastId });
+  //     } else {
+  //       toast.success("Semester created", { id: toastId });
+  //     }
+  //   } catch (err) {
+  //     toast.error("Something went wrong", { id: toastId });
+  //   }
+  };
+
   return (
-    <div>
-      <h2>Welcome to the CreateCourse page</h2>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="title" label="title" />
+          <PHInput type="text" name="prefix" label="prefix" />
+          <PHInput type="text" name="code" label="code" />
+          <PHInput type="text" name="credits" label="credits" />
+          <PHSelect mode="multiple" name="preRequisiteCourses"  options={preRequisiteCoursesOption} label="preRequisiteCourses"/>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
-
 export default CreateCourse;
--- src/redux/features/admin/courseManagement.api.ts
+++ src/redux/features/admin/courseManagement.api.ts
@@ -1,50 +1,80 @@
-import {  TQueryParam, TResponseRedux, tSemester } from "../../../types";
+import { TQueryParam, TResponseRedux, tSemester } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 const courseManagementApi = baseApi.injectEndpoints({
-   endpoints: (builder) => ({
-      getAllRegisteredSemesters: builder.query({
-        query: (args) => {
-          console.log(args);
-          const params = new URLSearchParams();
-  
-          if (args) {
-            args.forEach((item: TQueryParam) => {
-              params.append(item.name, item.value as string);
-            });
-          }
-  
-          return {
-            url: '/semester-registrations',
-            method: 'GET',
-            params: params,
-          };
-        },
-        providesTags:['semester'],
-        transformResponse: (response: TResponseRedux<tSemester[]>) => {
-          return {
-            data: response.data,
-            meta: response.meta,
-          };
-        },
-      }),
-      addRegisterSemester: builder.mutation({
-        query: (data) => ({
-          url: '/semester-registrations/create-semester-registration',
-          method: 'POST',
-          body: data,
-        }),
-        invalidatesTags:['semester']
-      }),
-      updateRegisterSemester: builder.mutation({
-        query: (args) => ({
-          url: `/semester-registrations/${args.id}`,
-          method: 'PATCH',
-          body: args.data,
-        }),
-        invalidatesTags:['semester']
-      }),
-    }),
-})
+  endpoints: (builder) => ({
+    getAllRegisteredSemesters: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
 
-export const {useUpdateRegisterSemesterMutation,useAddRegisterSemesterMutation, useGetAllRegisteredSemestersQuery} = courseManagementApi;
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/semester-registrations",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["semester"],
+      transformResponse: (response: TResponseRedux<tSemester[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addRegisterSemester: builder.mutation({
+      query: (data) => ({
+        url: "/semester-registrations/create-semester-registration",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["semester"],
+    }),
+    updateRegisterSemester: builder.mutation({
+      query: (args) => ({
+        url: `/semester-registrations/${args.id}`,
+        method: "PATCH",
+        body: args.data,
+      }),
+      invalidatesTags: ["semester"],
+    }),
+    getAllCourses: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["semester"],
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetAllCoursesQuery,
+  useUpdateRegisterSemesterMutation,
+  useAddRegisterSemesterMutation,
+  useGetAllRegisteredSemestersQuery,
+} = courseManagementApi;
